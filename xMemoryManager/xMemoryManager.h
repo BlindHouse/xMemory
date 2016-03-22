33becc29dbbd0eadf2b75b51ea3ed50a1958004c
DiSoJi <diegosolisj@gmail.com>
2016-03-21 23:30:07 -0600

    Se crean la clase del xMemoryNode

---
+++ xMemoryManager/xMemoryManager.h
@@ -0,0 +1,14 @@
+//
+// Created by disoji on 21/03/16.
+//
+
+#ifndef XMEMORY_XMEMORYMANAGER_H
+#define XMEMORY_XMEMORYMANAGER_H
+
+
+class xMemoryManager {
+
+};
+
+
+#endif //XMEMORY_XMEMORYMANAGER_H
