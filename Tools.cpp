bb9546134b78bc44147110d5001a5121049c168d
DiSoJi <diegosolisj@gmail.com>
2016-03-23 02:15:21 -0600

    Función char* getClassName() del xObject terminada

---
+++ Tools.cpp
@@ -0,0 +1,5 @@
+//
+// Created by disoji on 23/03/16.
+//
+
+#include "Tools.h"
---
+++ Tools.h
@@ -0,0 +1,14 @@
+//
+// Created by disoji on 23/03/16.
+//
+
+#ifndef XMEMORY_TOOLS_H
+#define XMEMORY_TOOLS_H
+
+
+class Tools {
+
+};
+
+
+#endif //XMEMORY_TOOLS_H
