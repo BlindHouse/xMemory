61a740da8279ed824ed8baaed38ce57fd9826706
DiSoJi <diegosolisj@gmail.com>
2016-04-13 05:59:39 -0600

    Añadida una lista simple especial para el proceso de serializacion

--- Tools.cpp
+++ Tools.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Tools.h"
+
+xLinkedList::xLinkedList() {
+    this->firstnode = NULL;
+    this->lastnode = NULL;
+    this->size = 0;
+    this->xclassname = "";
+}
+
+xLnode::xLnode(string NodeString) {
+    this->NodeString = NodeString;
+    this->next = NULL;
+    this->prev = NULL;
+}
+
+void xLinkedList::addNode(string StringToAdd) {
+    //cout<<"Before first node comprobation";
+    if (this->firstnode == NULL){
+        xLnode* tempNode = new xLnode(StringToAdd);
+        this->firstnode = tempNode;
+        this->lastnode = this->firstnode;
+        this->size ++;
+        //cout<<"After the asignation"<<"\n";
+    }else{
+        xLnode* tempNode = new xLnode(StringToAdd);
+        this->lastnode->next = tempNode;
+        this->lastnode = tempNode;
+        this->size ++;
+        }
+    }
+
+
+int xLinkedList::getSize() {
+    xLnode* temp = this->firstnode;
+    int cont = 0;
+    for (temp; temp!=NULL; temp = temp->next){
+        cont++;
+    }
+    return cont;
+}
--- Tools.h
+++ Tools.h
@@ -5,8 +5,34 @@
 #ifndef XMEMORY_TOOLS_H
 #define XMEMORY_TOOLS_H
 
+#include "string.h"
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include "stdlib.h"
+/*Recordar: Hay que hacer que se pueda copiar la memoria y poder manipularla ya que nos estaba dando error de segmentation fault
+ * Probar poniendo tódo como const char** a ver si así se puede manipular la memoria copiada, si no es mejor modular cada funcion del
+ * preprocesador y ejecutarla por linea.*/
+using namespace std;
 
-class Tools {
+class xLnode{
+public:
+    xLnode(string stringNode);
+    xLnode* next;
+    xLnode* prev;
+    string NodeString;
+
+};
+
+class xLinkedList{
+public:
+    int size;
+    string xclassname;
+    xLinkedList();
+    xLnode* firstnode;
+    xLnode* lastnode;
+    void addNode(string stringToAdd);
+    int getSize();
 
 };
 
